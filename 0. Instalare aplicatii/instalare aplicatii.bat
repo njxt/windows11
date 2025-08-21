126253452031923b7b7d7e1c3e5efb91dbb4cc76
njxt <66086654+njxt@users.noreply.github.com>
2025-08-21 18:57:55 +0300

    Add files via upload

--- 0. Instalare aplicatii/instalare aplicatii.bat
+++ 0. Instalare aplicatii/instalare aplicatii.bat
@@ -1,13 +1,12 @@
 @echo off
-echo Installing 7-Zip, qBittorrent, Discord, Spotify, Steam, DirectX, and Firefox...
+echo Installing Firefox, DirectX, 7-Zip, qBittorrent, Spotify, and .NET Framework...
 
+winget install --id Mozilla.Firefox -e --silent
+winget install --id Microsoft.DirectX -e --silent
 winget install --id 7zip.7zip -e --silent
 winget install --id qBittorrent.qBittorrent -e --silent
-winget install --id Discord.Discord -e --silent
 winget install --id Spotify.Spotify -e --silent
-winget install --id Valve.Steam -e --silent
-winget install --id Microsoft.DirectX -e --silent
-winget install --id Mozilla.Firefox -e --silent
+winget install --id Microsoft.DotNet.Framework.DeveloperPack_4 -e --silent
 
 echo Done!
 pause
